0f3e193154e5b888fe1e4c6ab8bd2cdc08644ca9
ruizhipu <18651885620@163.com>
2023-05-29 16:31:13 +0800

    add slurm file name for train.py

---
+++ imdb_wiki/slurm_script/run_contrastive.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=baselines
+#SBATCH --account=def-boyuwang
+#SBATCH --time=00-20:00
+#SBATCH --mem=32G
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=6
+#SBATCH --gres=gpu:1
+#SBATCH --mail-user=18651885620@163.com
+#SBATCH --mail-type=FAIL
+#SBATCH --output=slurm_output/slurm-%j-%x.out
+module load StdEnv/2020 cuda scipy-stack python/3.8
+#
+source /home/ruizhipu/envs/py38/bin/activate
+
+echo "SLURM_JOBID: " $SLURM_JOBID
+echo "SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
+echo "SLURM_ARRAY_JOB_ID: " $SLURM_ARRAY_JOB_ID
+
+
+
+python train.py --model_depth 50 --groups 25 --sigma 0.5 --la True --tua 0.5 --lr 0.001 --ranked_contra True --temp 5 --data_dir /home/ruizhipu/scratch/regression/imbalanced-regression/imdb-wiki-dir/data 
