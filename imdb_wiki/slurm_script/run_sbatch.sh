7acdfe96859137da4f9633215c5fa787ac3527fb
ruizhipu <18651885620@163.com>
2023-06-28 00:54:12 +0800

    add scripts for batch run

--- imdb_wiki/slurm_script/run_sbatch.sh
+++ imdb_wiki/slurm_script/run_sbatch.sh
@@ -4,7 +4,7 @@ for i in 0.0001 0.0002; do
                 for temp in 1 3 5 10; do
                     jobs='lr'_${i}_'tau_0.5'_'group'_${g}_'epoch'_${e}_'temp'_${temp}
                     echo ${jobs}
-                    sbatch --job-name=${jobs} run_train.sh ${i} ${g} ${e} ${temp}
+                    sbatch --job-name=${jobs} train.sh ${i} ${g} ${e} ${temp}
                 done
             done
     done
