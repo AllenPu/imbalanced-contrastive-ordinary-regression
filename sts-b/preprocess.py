473e98d75cc2b8d590e00c5a630b9953a8c49302
ruizhipu <18651885620@163.com>
2023-08-15 15:52:22 +0800

    test debug

--- sts-b/preprocess.py
+++ sts-b/preprocess.py
@@ -1,7 +1,7 @@
 '''Preprocessing functions and pipeline'''
 import nltk
-#nltk.download('punkt')
-nltk.word_tokenize("A pivot is the wpin or the central point")
+nltk.download('punkt')
+#nltk.word_tokenize("A pivot is the wpin or the central point")
 import torch
 import logging
 import numpy as np
