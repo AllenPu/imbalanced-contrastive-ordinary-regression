ce5f7195d4a6450b807442356275a752c7088fca
ruizhipu <18651885620@163.com>
2023-08-16 00:26:16 +0800

    test debug

--- sts-b/models.py
+++ sts-b/models.py
@@ -106,6 +106,7 @@ class MultiTaskModel(nn.Module):
         input1 = {key : input1[key].cuda() for key in input1}
         input2 = {key : input2[key].cuda() for key in input2}
         label = label.cuda()
+        print(mask1)
         if not mask1 and not mask2 :
             mask1 = {key: mask1[key].cuda() for key in mask1}
             imask2 = {key : mask2[key].cuda() for key in mask2}
