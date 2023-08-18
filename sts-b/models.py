5db52811ccb9d841532b6a0f2e51361bb525369c
ruizhipu <18651885620@163.com>
2023-08-18 20:33:57 +0800

    debug

--- sts-b/models.py
+++ sts-b/models.py
@@ -159,13 +159,13 @@ class MultiTaskModel(nn.Module):
                         self, 'regressor_%s_pred_layer' % group_gt[i].item())
                     output_gt = pred_layer_gt(pair_emb_s[i])
                     pred_list_gt.append(output_gt)
-                print(' current index for pred is {} for gt is {}'.format(group_hat[i].item(), group_gt[i].item()))
-                print(' current output for pred is {} for gt is {}'.format(output_.item(), output_gt.item()))
-                print('pred_layer_', pred_layer_ == pred_layer_gt)
-                print('name', 'regressor_%s_pred_layer' % group_hat[i].item(
-                ), ' name gt ', 'regressor_%s_pred_layer' % group_gt[i].item())
-                print('task name ', task.name, type(task.name),
-                      ' type fot gt ', type(group_gt[i].item()))
+                #print(' current index for pred is {} for gt is {}'.format(group_hat[i].item(), group_gt[i].item()))
+                #print(' current output for pred is {} for gt is {}'.format(output_.item(), output_gt.item()))
+                #print('pred_layer_', pred_layer_ == pred_layer_gt)
+                #print('name', 'regressor_%s_pred_layer' % group_hat[i].item(
+                #), ' name gt ', 'regressor_%s_pred_layer' % group_gt[i].item())
+                #print('task name ', task.name, type(task.name),
+                #      ' type fot gt ', type(group_gt[i].item()))
                 logits_gt = torch.cat(pred_list_gt)
             
 
