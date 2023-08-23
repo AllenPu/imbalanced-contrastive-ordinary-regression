ffc83def66479f6bc2f64b214572acb2090ed077
ruizhipu <18651885620@163.com>
2023-08-23 22:01:10 +0800

    debug

--- sts-b/slurm_jobs/run.sh
+++ sts-b/slurm_jobs/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=baselines
 #SBATCH --account=def-boyuwang
-#SBATCH --time=01-15:00
+#SBATCH --time=00-05:00
 #SBATCH --mem=32G
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=6
