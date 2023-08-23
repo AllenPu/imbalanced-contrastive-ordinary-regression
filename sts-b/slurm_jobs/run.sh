b60d05e09593ecd4c7d27d32e4607b2b2c186ce5
ruizhipu <18651885620@163.com>
2023-08-24 02:11:18 +0800

    change envs

--- sts-b/slurm_jobs/run.sh
+++ sts-b/slurm_jobs/run.sh
@@ -9,9 +9,9 @@
 #SBATCH --mail-user=18651885620@163.com
 #SBATCH --mail-type=FAIL
 #SBATCH --output=slurm_output/slurm-%j-%x.out
-module load StdEnv/2020 cuda scipy-stack python/3.8
+module load StdEnv/2020 cuda scipy-stack python/3.7
 #
-source /home/ruizhipu/envs/py38/bin/activate
+source /home/ruizhipu/sts/bin/activate
 
 echo "SLURM_JOBID: " $SLURM_JOBID
 echo "SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
