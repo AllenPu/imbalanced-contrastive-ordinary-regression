8ab0aa028a2ca2f34020b0f3066636bec6d578d4
ruizhipu <18651885620@163.com>
2023-08-24 01:37:54 +0800

    debug

--- sts-b/slurm_jobs/batch.sh
+++ sts-b/slurm_jobs/batch.sh
@@ -1,6 +1,6 @@
 for i in 1e-4 1e-3 5e-5 1e-5; do
     for g in 5 10 20 25; do
-        jobs='lr'_${i}_'group'_${g}
+        jobs='lr'_${i}_'groups'_${g}
         echo ${jobs}
         sbatch --job-name=${jobs} run.sh ${i} ${g}
     done
--- sts-b/slurm_jobs/run.sh
+++ sts-b/slurm_jobs/run.sh
@@ -16,6 +16,7 @@ source /home/ruizhipu/envs/py38/bin/activate
 echo "SLURM_JOBID: " $SLURM_JOBID
 echo "SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
 echo "SLURM_ARRAY_JOB_ID: " $SLURM_ARRAY_JOB_ID
-
+echo "current lr status : " $1
+echo "current groups status : " $2
 
 python train.py --group_wise --lr $1  --groups $2
