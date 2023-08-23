9440d982dbb836d7b8fcd887a924826af1f25376
ruizhipu <18651885620@163.com>
2023-08-23 21:58:32 +0800

    debug

--- sts-b/slurm_jobs/batch.sh
+++ sts-b/slurm_jobs/batch.sh
@@ -2,6 +2,6 @@ for i in 1e-4 1e-3 5e-5 1e-5; do
     for g in 5 10 20 25; do
         jobs='lr'_${i}_'group'_${g}
         echo ${jobs}
-        sbatch --job-name=${jobs} train.sh ${i} ${g}
+        sbatch --job-name=${jobs} run.sh ${i} ${g}
     done
 done
