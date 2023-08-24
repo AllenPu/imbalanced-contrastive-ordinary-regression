1f0a3d62724e01eea369717dcb7f086247a3eb59
ruizhipu <18651885620@163.com>
2023-08-24 16:00:04 +0800

    change the path to output

--- sts-b/slurm_jobs/run.sh
+++ sts-b/slurm_jobs/run.sh
@@ -8,7 +8,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mail-user=18651885620@163.com
 #SBATCH --mail-type=FAIL
-#SBATCH --output=slurm_output/slurm-%j-%x.out
+#SBATCH --output=/home/ruizhipu/scratch/regression/imbalanced-contrastive-ordinary-regression/sts-b/slurm_output/slurm-%j-%x.out
 module load StdEnv/2020 cuda scipy-stack python/3.7
 #
 source /home/ruizhipu/sts/bin/activate
