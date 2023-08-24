487888ed8a780485bd41afdaf91a153afcbd51c1
ruizhipu <18651885620@163.com>
2023-08-25 01:43:49 +0800

    change param

--- sts-b/batch.sh
+++ sts-b/batch.sh
@@ -1,7 +1,9 @@
-for i in 1e-4 1e-3 5e-5 1e-5; do
+for i in 1e-3 5e-3 1e-4; do
     for g in 5 10 20 25; do
-        jobs='lr'_${i}_'groups'_${g}
-        echo ${jobs}
-        sbatch --job-name=${jobs} ./slurm_jobs/run.sh ${i} ${g}
+        for s in 0.5 1 1.5 2; do
+            jobs='lr'_${i}_'groups'_${g}_'sigma'_${s}
+            echo ${jobs}
+            sbatch --job-name=${jobs} ./slurm_jobs/run.sh ${i} ${g} ${s}
+        done
     done
 done
