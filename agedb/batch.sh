7fc528ad21c789317d002c80c6d7bea91441ba1f
ruizhipu <18651885620@163.com>
2023-08-30 20:58:59 +0800

    change param to batch.sh in agedb

--- agedb/batch.sh
+++ agedb/batch.sh
@@ -2,7 +2,7 @@ for i in 0.0001 0.0005 0.00001; do
     for g in 10 20 25; do
         for e in 100 150 200; do
             for sigma in 0.5 1 1.5;do
-                for contra in True False; do
+                for contra in False; do
                     if contra;then
                         for temp in 0.07 0.1 0.5; do
                             jobs='contra_'+'lr'_${i}_'tau_0.5'_'group'_${g}_'epoch'_${e}_'temp'_${temp}
