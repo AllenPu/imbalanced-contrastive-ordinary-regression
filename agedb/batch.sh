6170f7b0939a879142f5679420089015b88dd5e2
ruizhipu <18651885620@163.com>
2023-09-01 13:27:45 +0800

    debug in agedb/batch

--- agedb/batch.sh
+++ agedb/batch.sh
@@ -4,7 +4,7 @@ for i in 0.0001 0.0003; do
             for sigma in 0.5 1 1.5;do
                 for contra in True; do
                     if [ $contra ];then
-                        for temp in 0.07 0.1 0.5 0.05; do
+                        for temp in 0.07 0.1 0.05; do
                             jobs='contra_'+'lr'_${i}_'tau_0.5'_'group'_${g}_'epoch'_${e}_'temp'_${temp}
                             echo ${jobs}
                             sbatch --job-name=${jobs} ./slurm_jobs/train_contra.sh ${i} ${g} ${e} ${temp} ${sigma}
