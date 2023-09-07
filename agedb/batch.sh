6a614ace53e8e804ebbe15740884fd911efa4b28
ruizhipu <18651885620@163.com>
2023-09-07 22:12:52 +0800

    change param

--- agedb/batch.sh
+++ agedb/batch.sh
@@ -1,10 +1,10 @@
 for i in 0.0003; do
     for g in 10 20; do
         for e in 300 350; do
-            for sigma in 0.1 0.4 2;do
+            for sigma in 2 3 4;do
                 for contra in True; do
                     if [ $contra ];then
-                        for temp in 0.01 0.05; do
+                        for temp in 0.03 0.04; do
                             jobs='contra_'+'lr'_${i}_'tau_0.5'_'group'_${g}_'epoch'_${e}_'temp'_${temp}
                             echo ${jobs}
                             sbatch --job-name=${jobs} ./slurm_jobs/train_contra.sh ${i} ${g} ${e} ${temp} ${sigma}
