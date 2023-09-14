1ce76e80fb3d6091bf94d03ad40f1fb89d43413f
ruizhipu <18651885620@163.com>
2023-09-14 23:37:44 +0800

    add epoch to sts

--- sts-b/batch.sh
+++ sts-b/batch.sh
@@ -1,9 +1,11 @@
 for i in 1e-3 5e-3 5e-4 1e-4; do
     for g in 5 10 20 25; do
         for s in 0.5 1 1.5 2; do
-            jobs='lr'_${i}_'groups'_${g}_'sigma'_${s}
-            echo ${jobs}
-            sbatch --job-name=${jobs} ./slurm_jobs/run.sh ${i} ${g} ${s}
+            for e in 100 200 300:do
+                jobs='lr'_${i}_'groups'_${g}_'sigma'_${s}_'epoch'_${e}
+                echo ${jobs}
+                sbatch --job-name=${jobs} ./slurm_jobs/run.sh ${i} ${g} ${s} ${e}
+            done
         done
     done
 done
