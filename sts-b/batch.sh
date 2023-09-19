b6d9eb3eb79d6140a1257120d16a4ddc5e40d8ad
ruizhipu <18651885620@163.com>
2023-09-20 02:29:20 +0800

    add batch run in sts

--- sts-b/batch.sh
+++ sts-b/batch.sh
@@ -3,11 +3,15 @@ for i in 1e-3 5e-3 5e-4 1e-4; do
         for s in 1 2; do
             for e in 100 200 300:do
                 for la in True False; do
-                    if [ $la ];then
+                    if [ $la ] ;then
                         jobs='la_true_contra_'+'lr'_${i}_'tau_0.5'_'group'_${g}_'epoch'_${e}
                         echo ${jobs}
                         sbatch --job-name=${jobs} ./slurm_jobs/run_la.sh ${i} ${g} ${e} ${sigma}
-                        
+                        for temp in 0.07; do
+                            jobs='la_true_contra_'+'lr'_${i}_'tau_0.5'_'group'_${g}_'epoch'_${e}_'temp'+${temp}
+                            echo ${jobs}
+                            sbatch --job-name=${jobs} ./slurm_jobs/run_contra.sh ${i} ${g} ${e} ${sigma} ${temp}
+                        done
                     else
                         jobs='la_false_lr'_${i}_'groups'_${g}_'sigma'_${s}_'epoch'_${e}
                         echo ${jobs}
