171ec23742919b199a1654803e387f35c15e82a2
ruizhipu <18651885620@163.com>
2023-09-27 01:33:28 +0800

    add epoch

--- sts-b/batch.sh
+++ sts-b/batch.sh
@@ -1,7 +1,7 @@
 for i in 1e-3 5e-3 5e-4 1e-4; do
     for g in 5 10 20; do
         for s in 1 2; do
-            for e in 100 200 300; do
+            for e in 300 400 500; do
                 for la in True False; do
                     if [ $la ] ;then
                         jobs='la_true_contra_lr'_${i}_'tau_0.5'_'group'_${g}_'epoch'_${e}_sigma_${s}
