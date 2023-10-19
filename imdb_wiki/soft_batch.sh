2abea02fa8f42d099ff1ae3e2abb6720071e57d7
AllenPu <18651885620@163.com>
2023-10-19 15:36:50 -0400

    debug

--- imdb_wiki/soft_batch.sh
+++ imdb_wiki/soft_batch.sh
@@ -5,7 +5,7 @@ for i in 0.0001 0.0005 0.00001; do
                 for sgima in 1 1.5 2; do
                     jobs='lr'_${i}_'tau_0.5'_'group'_${g}_'epoch'_${e}_'temp'_${temp}
                     echo ${jobs}
-                    sbatch --job-name=${jobs} ./slurm_jobs/soft.sh ${i} ${g} ${e} ${temp} ${sigma}
+                    sbatch --job-name=${jobs} ./slurm_script/soft.sh ${i} ${g} ${e} ${temp} ${sigma}
                 done
             done
         done
