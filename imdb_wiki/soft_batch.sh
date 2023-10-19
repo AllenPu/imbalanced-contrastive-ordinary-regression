b322c46c4f134df131c04b70fcf5b48a3be7a1ee
AllenPu <18651885620@163.com>
2023-10-19 15:34:49 -0400

    add bacth train in slurm for imdb

--- imdb_wiki/soft_batch.sh
+++ imdb_wiki/soft_batch.sh
@@ -1,10 +1,12 @@
 for i in 0.0001 0.0005 0.00001; do
-    for g in 10 20 25; do
-        for e in 100 150 160; do
-            for temp in 0.07 0.1 0.5; do
+    for g in 10 20 ; do
+        for e in 200 250; do
+            for temp in 0.02 0.05; do
+                for sgima in 1 1.5 2; do
                     jobs='lr'_${i}_'tau_0.5'_'group'_${g}_'epoch'_${e}_'temp'_${temp}
                     echo ${jobs}
-                    sbatch --job-name=${jobs} ./slurm_jobs/soft.sh ${i} ${g} ${e} ${temp}
+                    sbatch --job-name=${jobs} ./slurm_jobs/soft.sh ${i} ${g} ${e} ${temp} ${sigma}
+                done
             done
         done
     done
