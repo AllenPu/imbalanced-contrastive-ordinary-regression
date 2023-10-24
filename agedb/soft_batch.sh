d5ed9e5521e523f46ad4608820a81a1459e5f707
AllenPu <18651885620@163.com>
2023-10-24 16:50:09 -0400

    add

--- agedb/soft_batch.sh
+++ agedb/soft_batch.sh
@@ -5,12 +5,12 @@ for i in 0.001 0.002 0.003; do
                 for contra in True; do
                     if [ $contra ];then
                         for temp in 0.02 0.05 0.07; do
-                            jobs='contra_'+'lr'_${i}_'tau_0.5'_'group'_${g}_'epoch'_${e}_'temp'_${temp}
+                            jobs='contra_'+'lr'_${i}_'tau_1'_'group'_${g}_'epoch'_${e}_'temp'_${temp}
                             echo ${jobs}
                             sbatch --job-name=${jobs} ./slurm_jobs/uncertain.sh ${i} ${g} ${e} ${temp} ${sigma}
                         done
                     else
-                        jobs='lr'_${i}_'tau_0.5'_'group'_${g}_'epoch'_${e}
+                        jobs='lr'_${i}_'tau_1'_'group'_${g}_'epoch'_${e}
                         echo ${jobs}
                         #sbatch --job-name=${jobs} ./slurm_jobs/train.sh ${i} ${g} ${e} ${sigma}
                     fi 
--- imdb_wiki/soft_batch.sh
+++ imdb_wiki/soft_batch.sh
@@ -3,7 +3,7 @@ for i in 0.0001 0.0005 0.00001; do
         for e in 200 250; do
             for temp in 0.02 0.05; do
                 for sgima in 1 1.5 2; do
-                    jobs='lr'_${i}_'tau_0.5'_'group'_${g}_'epoch'_${e}_'temp'_${temp}
+                    jobs='lr'_${i}_'tau_1'_'group'_${g}_'epoch'_${e}_'temp'_${temp}
                     echo ${jobs}
                     sbatch --job-name=${jobs} ./slurm_script/soft.sh ${i} ${g} ${e} ${temp} ${sigma}
                 done
