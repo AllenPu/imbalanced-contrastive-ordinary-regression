940f68333241e7b9c330be45c154af200a56a358
ruizhipu <18651885620@163.com>
2023-11-01 14:27:36 +0800

    add exp to agedb

--- agedb/soft_batch.sh
+++ agedb/soft_batch.sh
@@ -1,7 +1,7 @@
 for i in 0.001 0.0015; do
-    for g in 25; do
+    for g in 20; do
         for e in 300; do
-            for sigma in 1 1.5 2 2.5;do
+            for sigma in 1 1.5 2;do
                 for contra in True; do
                     if [ $contra ];then
                         for temp in 0.02 0.05 0.07 0.1; do
