c3bda93fad4bd1a08d1304b24570cd951daf38ca
AllenPu <18651885620@163.com>
2023-11-01 22:40:12 -0400

    add

--- imdb_wiki/slurm_script/soft_single_job.sh
+++ imdb_wiki/slurm_script/soft_single_job.sh
@@ -1,1 +1,1 @@
-python train.py --lr 0.0001 --sigma 1.5 --epoch 200 --soft_label  --ce --groups 10 --ranked_contra --temp 0.02 --output_file soft_imdb_1101_ --data_dir /home/rpu2/scratch/data/imbalanced-regression/imdb-wiki-dir/data
+CUDA_VISIBLE_DEVICES=1 python train.py --lr 0.0001 --sigma 1.5 --epoch 200 --soft_label  --ce --groups 10 --ranked_contra --temp 0.02 --output_file soft_imdb_1101_ --data_dir /home/rpu2/scratch/data/imbalanced-regression/imdb-wiki-dir/data
