9419e2e9154aff36dfdbce86ec293c77d5066fc9
ruizhipu <18651885620@163.com>
2023-11-06 14:18:46 +0800

    add no contra to imdb

--- imdb_wiki/soft_batch.sh
+++ imdb_wiki/soft_batch.sh
@@ -5,15 +5,16 @@ for i in 0.0001 0.0005; do
                 for contra in True; do
                     if [ $contra ];then
                         for temp in 0.02 0.05; do
-                        jobs='imdb_wiki_lr'_${i}_'tau_1'_'group'_${g}_'epoch'_${e}_'temp'_${temp}
+                            jobs='imdb_wiki_lr'_${i}_'tau_1'_'group'_${g}_'epoch'_${e}_'temp'_${temp}
+                            echo ${jobs}
+                            sbatch --job-name=${jobs} ./slurm_script/soft.sh ${i} ${g} ${e} ${temp} ${sigma}
+                        done
+                    else
+                        jobs='imdb_wiki_lr_lr'_${i}_'tau_0.5'_'group'_${g}_'epoch'_${e}
                         echo ${jobs}
-                        sbatch --job-name=${jobs} ./slurm_script/soft.sh ${i} ${g} ${e} ${temp} ${sigma}
-                    done
-                else
-                    jobs='imdb_wiki_lr_lr'_${i}_'tau_0.5'_'group'_${g}_'epoch'_${e}
-                    echo ${jobs}
-                    sbatch --job-name=${jobs} ./slurm_script/train.sh ${i} ${g} ${e} ${sigma}
-                fi
+                        sbatch --job-name=${jobs} ./slurm_script/train.sh ${i} ${g} ${e} ${sigma}
+                    fi
+                done
             done
         done
     done
