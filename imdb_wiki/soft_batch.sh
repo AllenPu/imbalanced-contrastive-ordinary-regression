a2657c4cc69fe4a5b27abe40e3f202047754b2c8
ruizhipu <18651885620@163.com>
2023-11-06 15:17:19 +0800

    add no contra to imdb

--- imdb_wiki/soft_batch.sh
+++ imdb_wiki/soft_batch.sh
@@ -2,7 +2,7 @@ for i in 0.0001 0.0005; do
     for g in 10; do
         for e in 200; do
             for sigma in 1 1.5 2; do
-                for contra in True; do
+                for contra in False; do
                     if [ $contra ];then
                         for temp in 0.02 0.05; do
                             jobs='imdb_wiki_lr'_${i}_'tau_1'_'group'_${g}_'epoch'_${e}_'temp'_${temp}
