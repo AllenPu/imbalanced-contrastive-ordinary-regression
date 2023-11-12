665d26f782aa0333b9e7ad06e9fc4cf5fb4f0ae8
AllenPu <18651885620@163.com>
2023-11-11 22:25:16 -0500

    debug

--- sts-b/soft_batch.sh
+++ sts-b/soft_batch.sh
@@ -1,8 +1,8 @@
 for i in 0.0001 0.0005; do
     for g in 10 20; do
-        for p in 400 500; do
+        for p in 200 300; do
             for temp in 0.02 0.05 0.07; do
-                for sigma in 0.7 1 1.5 2; do
+                for sigma in 0.7 1 1.5; do
                     jobs='sts'_${i}_'group'_${g}_'epoch'_${e}_'temp'_${temp}
                     echo ${jobs}
                     sbatch --job-name=${jobs} ./slurm_jobs/soft.sh ${i} ${g} ${p} ${temp} ${sigma}
