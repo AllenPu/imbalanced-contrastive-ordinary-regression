5d940e3e294b51bea90ff5df01533e2ec9cd20fa
AllenPu <18651885620@163.com>
2023-11-12 21:05:31 -0500

    debug

--- sts-b/soft_batch.sh
+++ sts-b/soft_batch.sh
@@ -1,6 +1,6 @@
 for i in 0.0001 0.0005; do
     for g in 10 20; do
-        for p in 200 300; do
+        for p in 200; do
             for temp in 0.02 0.05 0.07; do
                 for sigma in 0.7 1 1.5; do
                     jobs='sts'_${i}_'group'_${g}_'epoch'_${e}_'temp'_${temp}
