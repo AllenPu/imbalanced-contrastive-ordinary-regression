57f7e0da02874cb110ffbc2a4dfe2702bd7d8526
AllenPu <18651885620@163.com>
2023-11-13 22:30:09 -0500

    debug

--- imdb_wiki/soft_batch.sh
+++ imdb_wiki/soft_batch.sh
@@ -1,8 +1,8 @@
-for i in 0.0001 0.0005; do
-    for g in 10; do
-        for e in 200; do
+for i in 0.00005; do
+    for g in 20; do
+        for e in 90; do
             for sigma in 1 1.5 2; do
-                for contra in False; do
+                for contra in True; do
                     if [ $contra ];then
                         for temp in 0.02 0.05; do
                             jobs='imdb_wiki_lr'_${i}_'tau_1'_'group'_${g}_'epoch'_${e}_'temp'_${temp}
