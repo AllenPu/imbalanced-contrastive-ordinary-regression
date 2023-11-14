e554b5f358b664ecefd8f0084d31e74dcc22b057
AllenPu <18651885620@163.com>
2023-11-13 21:56:06 -0500

    debug

--- agedb/soft_batch.sh
+++ agedb/soft_batch.sh
@@ -1,10 +1,10 @@
-for i in 0.001 0.0015; do
+for i in 0.0012 0.0017; do
     for g in 20; do
         for e in 300; do
-            for sigma in 1 1.5 2;do
+            for sigma in 1 2;do
                 for contra in True; do
                     if [ $contra ];then
-                        for temp in 0.02 0.05 0.07 0.1; do
+                        for temp in 0.02 0.07; do
                             jobs='agedb_contra_lr'_${i}_'tau_1'_'group'_${g}_'epoch'_${e}_'temp'_${temp}
                             echo ${jobs}
                             sbatch --job-name=${jobs} ./slurm_jobs/uncertain.sh ${i} ${g} ${e} ${temp} ${sigma}
