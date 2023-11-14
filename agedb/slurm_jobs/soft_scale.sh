478fc981e99afabe7ec60d6509ae481d6f5359af
AllenPu <18651885620@163.com>
2023-11-13 21:39:18 -0500

    debug

--- agedb/slurm_jobs/soft_scale.sh
+++ agedb/slurm_jobs/soft_scale.sh
@@ -20,5 +20,5 @@ echo "SLURM_ARRAY_JOB_ID: " $SLURM_ARRAY_JOB_ID
 
 #python train.py --la --tau 0.5 --lr 0.0002 --temp 0.03 --sigma 1 --epoch 200 --ranked_contra --soft_label --output_file softlabel_
 #python train.py --lr 0.001 --groups 10 --epoch 250 --temp 0.05 --sigma 1 --ranked_contra --soft_label --ce --scale 1.2 --output_file softlabel_scale1.2_1109_
-python train.py --lr 0.0015 --groups 20 --epoch 250 --temp 0.02 --sigma 2 --ranked_contra --soft_label --ce --diversity 0.2 --output_file softlabel_diversity0.2_1113_
+python train.py --lr 0.0015 --groups 20 --epoch 250 --temp 0.02 --sigma 2 --ranked_contra --soft_label --ce --diversity 0.5 --output_file softlabel_diversity0.5_1113_
 #python train.py --lr 0.001 --groups 10 --epoch 250 --temp 0.05 --sigma 1 --ranked_contra --soft_label --ce --scale 0.7 --output_file softlabel_scale0.7_1103_
