0b44bd8ce2c78caec65be3b569e0588d7c0c9ee0
AllenPu <18651885620@163.com>
2023-11-27 17:55:21 -0500

    add d to 0.1

--- agedb/slurm_jobs/soft_scale.sh
+++ agedb/slurm_jobs/soft_scale.sh
@@ -21,4 +21,4 @@ echo "SLURM_ARRAY_JOB_ID: " $SLURM_ARRAY_JOB_ID
 #python train.py --la --tau 0.5 --lr 0.0002 --temp 0.03 --sigma 1 --epoch 200 --ranked_contra --soft_label --output_file softlabel_
 #python train.py --lr 0.001 --groups 10 --epoch 250 --temp 0.05 --sigma 1 --ranked_contra --soft_label --ce --scale 1.2 --output_file softlabel_scale1.2_1109_
 #python train.py --lr 0.0015 --groups 20 --epoch 350 --temp 0.02 --sigma 2 --ranked_contra --soft_label --ce --diversity 0.1 --output_file softlabel_diversity0.1e350_1113_
-python train.py --lr 0.001 --groups 10 --epoch 250 --temp 0.05 --sigma 1 --ranked_contra --soft_label --ce --scale 0.95  --diversity 0.5 --output_file softlabel_s_0.95d0.5__1123_
+python train.py --lr 0.001 --groups 10 --epoch 250 --temp 0.05 --sigma 1 --ranked_contra --soft_label --ce --scale 0.95  --diversity 0.1 --output_file softlabel_s_0.95d0.1__1127_
