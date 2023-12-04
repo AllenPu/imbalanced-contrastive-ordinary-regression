0572622df65a32c6e68c769200f839170961c02f
ruizhipu <18651885620@163.com>
2023-12-04 02:52:20 -0500

    add cls head to nyud2

--- nyud2/models/net.py
+++ nyud2/models/net.py
@@ -15,23 +15,15 @@ class model(nn.Module):
         #self.classifier = modules.classifier_Regressor(args)
         self.input_shape = 0
         self.args = args
+        if args.addcls is True and args.cls_num > 0:
+            self.R = modules.Rcls(args, block_channel, args.cls_num)
+        else:
+            self.R = modules.R(args, block_channel)
 
     def forward(self, x, depth=None, epoch=None):
         x_block1, x_block2, x_block3, x_block4 = self.E(x)
         x_decoder = self.D(x_block1, x_block2, x_block3, x_block4)
         x_mff = self.MFF(x_block1, x_block2, x_block3, x_block4, [
                          x_decoder.size(2), x_decoder.size(3)])
-        if self.args.group_mode and self.input_shape == 0:
-            feat = torch.cat((x_decoder, x_mff), 1)
-            reg_output = self.R(feat, depth, epoch)
-            cls_output = self.classifier(torch.cat((x_decoder, x_mff), 1))
-            if self.input_shape == 0:
-                self.input_shape = self.R.input_shape
-                self.classifier = modules.classifier_Regressor(self.args, self.input_shape)
-                cls_output = self.classifier(feat)
-            else:
-                cls_output = self.classifier(feat)
-            return reg_output, cls_output
-        else:
-            out = self.R(torch.cat((x_decoder, x_mff), 1), depth, epoch)
-            return out
+        out = self.R(torch.cat((x_decoder, x_mff), 1), depth, epoch)
+        return out
