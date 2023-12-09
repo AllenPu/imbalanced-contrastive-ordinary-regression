5c92629df88a7504db95e955f714018c0a7827d8
ruizhipu <18651885620@163.com>
2023-12-09 02:00:51 -0500

    debug

--- agedb/train.py
+++ agedb/train.py
@@ -192,8 +192,8 @@ def train_one_epoch(model, train_loader, ce_loss, mse_loss, opt, args):
         if ranked_contra and not args.ce:
             loss_contra = contra_ratio * ce_loss(z, g)
             loss += loss_contra
-            ce = F.cross_entropy(g_pred, g.squeeze(-1).long())
-            print(f" loss contra is {loss_contra.item()} ce is {ce}")
+            #ce = F.cross_entropy(g_pred, g.squeeze(-1).long())
+            #print(f" loss contra is {loss_contra.item()} ce is {ce}")
 
         # add soft label based loss
         if args.soft_label:
