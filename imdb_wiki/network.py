10ca147f1226366239b91d91ad36dd6e30feeb93
ruizhipu <18651885620@163.com>
2023-12-17 01:19:23 -0500

    add a two tower mode

--- imdb_wiki/network.py
+++ imdb_wiki/network.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 import torchvision
 import torch
+import torch.optim as optim
 
 
 class ResNet_regression(nn.Module):
@@ -83,3 +84,52 @@ class ResNet_regression_ddp(nn.Module):
             g_index = torch.argmax(g_hat, dim=1).unsqueeze(-1)
             y_gt = torch.gather(y_hat_all, dim=1, index=g.to(torch.int64))
             return g_index, y_hat, y_gt
+        
+
+
+class ResNet_two_tower(nn.Module):
+    def __init__(self, args):
+        super(ResNet_regression, self).__init__()
+        self.groups = args.groups
+        exec('self.model = torchvision.models.resnet{}(pretrained=False)'.format(
+            args.model_depth))
+        #
+        output_dim = args.groups
+        #
+        fc_inputs = self.model.fc.in_features
+        #
+        self.model_extractor = nn.Sequential(*list(self.model.children())[:-1])
+        #
+        self.Flatten = nn.Flatten(start_dim=1)
+        #
+        self.model_cls = nn.Sequential(nn.Linear(fc_inputs, output_dim))
+        #
+        self.model_reg = nn.Sequential(nn.Linear(fc_inputs, output_dim))
+        #self.mode = args.mode
+        self.sigma = args.sigma
+
+    # g is the same shape of y
+    def forward(self, x, mode='cls'):
+        #"output of model dim is 2G"
+        z = self.model_extractor(x)
+        #
+        z = self.Flatten(z)
+        #
+        if mode == 'cls':
+            g_hat = self.model_linear(z)
+            return g_hat, z
+        elif mode == 'reg':
+            g_hat = self.model_linear(z)
+            y_hat = self.model_reg(z)
+            return g_hat, y_hat, z
+        else:
+            print(" No mode specify!!! ")
+     
+    def setup_opt(self, args):
+        opt_extractor = optim.adam(self.model_extractor.parameters(), lr=args.lr, weight_decay=5e-4)
+        opt_cls =  optim.adam(self.model_cls.parameters(), lr=args.lr, weight_decay=5e-4)
+        opt_reg = optim.adam(self.model_reg.parameters(),lr=args.lr, weight_decay=5e-4)
+        return opt_extractor, opt_cls, opt_reg
+
+    
+    
