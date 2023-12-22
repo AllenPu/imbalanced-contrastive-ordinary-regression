347d975c31d8143a3ba96b1b6a4e4294d2c67ec5
ruizhipu <18651885620@163.com>
2023-12-22 00:48:38 -0500

    debug

--- imdb_wiki/train.py
+++ imdb_wiki/train.py
@@ -201,8 +201,9 @@ def train_one_epoch(model, train_loader, ce_loss, mse_loss, opt, args, e=0):
             #print(f' shape of ce_g is {ce_g.shape} w shape is {w.shape}')
             ce_g_1 = (ce_g @w )/bs
             ce_g = ce_g * w.squeeze(-1)
-            print(f' ce_g_1 shape is {ce_g_1.shape} ce_g is {ce_g.shape}')
-            #ce_g = torch.mean(ce_g)
+            print(f' ce_g_1 shape is {ce_g_1.shape}')
+            ce_g = torch.mean(ce_g)
+            print(f'ce_g is {ce_g.shape} ce {ce_g}')
             loss_list.append(ce_g)
             #print(f'smooth loss is {ce_g.item()}')
         #
