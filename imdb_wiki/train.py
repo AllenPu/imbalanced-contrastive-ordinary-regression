1e7b85c76c6f9ab40b016f6888448ea3c0b14e72
ruizhipu <18651885620@163.com>
2023-12-22 01:09:51 -0500

    debug

--- imdb_wiki/train.py
+++ imdb_wiki/train.py
@@ -188,6 +188,7 @@ def train_one_epoch(model, train_loader, ce_loss, mse_loss, opt, args, e=0):
         else:
             mse_y = mse_loss(y_predicted, y)
         #
+        print(f' shape pof mse {mse_y.shape}')
         loss_list.append(sigma*mse_y)
         #
         if la:
@@ -199,11 +200,11 @@ def train_one_epoch(model, train_loader, ce_loss, mse_loss, opt, args, e=0):
         if args.smooth:
             ce_g = F.cross_entropy(g_hat, g.squeeze().long(), reduction='none')
             #print(f' shape of ce_g is {ce_g.shape} w shape is {w.shape}')
-            ce_g_1 = (ce_g @ w )/bs
+            #ce_g_1 = (ce_g @ w )/bs
             ce_g = ce_g * w.squeeze(-1)
-            print(f' ce_g_1 shape is {ce_g_1.shape}')
+            #print(f' ce_g_1 shape is {ce_g_1.shape}')
             ce_g = torch.mean(ce_g)
-            print(f'ce_g is {ce_g} type is {type(ce_g)}')
+            #print(f'ce_g is {ce_g} type is {type(ce_g)}')
             loss_list.append(ce_g)
             #print(f'smooth loss is {ce_g.item()}')
         #
