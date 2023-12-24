397eebd277138654bb8f02620b96cf76c5609f07
ruizhipu <18651885620@163.com>
2023-12-23 23:26:42 -0500

    write down

--- imdb_wiki/train.py
+++ imdb_wiki/train.py
@@ -555,6 +555,7 @@ if __name__ == '__main__':
     write_test_loggs('./output/'+store_name, results_test, shot_dict_pred,
                 shot_dict_gt, shot_dict_cls, args)
     #
+    # check if used  eq groups
     with open('test_gt_{}_eq.pkl', 'wb') as f:
         pickle.dump(group_and_pred, f)
     #
