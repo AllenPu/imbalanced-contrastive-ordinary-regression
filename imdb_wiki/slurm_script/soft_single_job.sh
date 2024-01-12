f305132b390cac6476055943d9ff4f8d715807e2
AllenPu <18651885620@163.com>
2024-01-12 18:44:33 -0500

    polish

--- imdb_wiki/slurm_script/soft_single_job.sh
+++ imdb_wiki/slurm_script/soft_single_job.sh
@@ -4,4 +4,4 @@
 #CUDA_VISIBLE_DEVICES=1 python train.py --lr 0.001 --groups 20 --epoch 300 --batch_size 256 --lds True --smooth True --output_file smooth_1229_ --data_dir /home/rpu2/scratch/data/imbalanced-regression/imdb-wiki-dir/data
 #CUDA_VISIBLE_DEVICES=2 python train.py --lr 0.0001 --group_mode b_g --groups 20 --epoch 200 --batch_size 256 --lds True --ce --output_file smooth_eq_1225 --data_dir /home/rpu2/scratch/data/imbalanced-regression/imdb-wiki-dir/data
 #CUDA_VISIBLE_DEVICES=1 python train.py --lr 0.0005 --groups 20 --epoch 300 --group_mode b_g --batch_size 256 --ranked_contra --ce --lds True --output_file bg_g20_1231 --data_dir /home/rpu2/scratch/data/imbalanced-regression/imdb-wiki-dir/data
-CUDA_VISIBLE_DEVICES=0 python train.py --lr 0.0001 --groups 20 --epoch 200 --aug True --output_file aug_ --data_dir /home/rpu2/scratch/data/imbalanced-regression/imdb-wiki-dir/data
+CUDA_VISIBLE_DEVICES=0 python train.py --lr 0.0001 --groups 20 --epoch 200 --aug --output_file aug_ --data_dir /home/rpu2/scratch/data/imbalanced-regression/imdb-wiki-dir/data
