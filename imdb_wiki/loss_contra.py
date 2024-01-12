d71d2cd24a8c76206a0cd93a3be2052529abfd26
ruizhipu <18651885620@163.com>
2024-01-12 00:48:25 -0500

    add pairwise contrastive

--- imdb_wiki/loss_contra.py
+++ imdb_wiki/loss_contra.py
@@ -96,3 +96,41 @@ class Softlabel_Loss(nn.Module):
         target = torch.clamp(target.float(), min=self.label_smooth/(self.class_num-1), max=1.0-self.label_smooth)
         loss = -1*torch.sum(target*logprobs, 1)
         return loss.mean()
+    
+
+class RnCLoss_pairwise(nn.Module):
+    def __init__(self, temperature=2, label_diff='l1', feature_sim='l2'):
+        super(RnCLoss, self).__init__()
+        self.t = temperature
+        self.label_diff_fn = LabelDifference(label_diff)
+        self.feature_sim_fn = FeatureSimilarity(feature_sim)
+
+    def forward(self, features, labels):
+        # features: [bs, 2, feat_dim]
+        # labels: [bs, label_dim]
+
+        features = torch.cat([features[:, 0], features[:, 1]], dim=0)  # [2bs, feat_dim]
+        labels = labels.repeat(2, 1)  # [2bs, label_dim]
+
+        label_diffs = self.label_diff_fn(labels)
+        logits = self.feature_sim_fn(features).div(self.t)
+        logits_max, _ = torch.max(logits, dim=1, keepdim=True)
+        logits -= logits_max.detach()
+        exp_logits = logits.exp()
+
+        n = logits.shape[0]  # n = 2bs
+
+        # remove diagonal
+        logits = logits.masked_select((1 - torch.eye(n).to(logits.device)).bool()).view(n, n - 1)
+        exp_logits = exp_logits.masked_select((1 - torch.eye(n).to(logits.device)).bool()).view(n, n - 1)
+        label_diffs = label_diffs.masked_select((1 - torch.eye(n).to(logits.device)).bool()).view(n, n - 1)
+
+        loss = 0.
+        for k in range(n - 1):
+            pos_logits = logits[:, k]  # 2bs
+            pos_label_diffs = label_diffs[:, k]  # 2bs
+            neg_mask = (label_diffs >= pos_label_diffs.view(-1, 1)).float()  # [2bs, 2bs - 1]
+            pos_log_probs = pos_logits - torch.log((neg_mask * exp_logits).sum(dim=-1))  # 2bs
+            loss += - (pos_log_probs / (n * (n - 1))).sum()
+
+        return loss
