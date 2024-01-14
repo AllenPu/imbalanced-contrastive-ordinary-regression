ed9f7e3b8ff9095ca81565369463289b4e0e42b7
AllenPu <18651885620@163.com>
2024-01-13 19:35:40 -0500

    sota

--- imdb_wiki/slurm_script/soft_single_job.sh
+++ imdb_wiki/slurm_script/soft_single_job.sh
@@ -5,4 +5,4 @@
 #CUDA_VISIBLE_DEVICES=2 python train.py --lr 0.0001 --group_mode b_g --groups 20 --epoch 200 --batch_size 256 --lds True --ce --output_file smooth_eq_1225 --data_dir /home/rpu2/scratch/data/imbalanced-regression/imdb-wiki-dir/data
 #CUDA_VISIBLE_DEVICES=1 python train.py --lr 0.0005 --groups 20 --epoch 300 --group_mode b_g --batch_size 256 --ranked_contra --ce --lds True --output_file bg_g20_1231 --data_dir /home/rpu2/scratch/data/imbalanced-regression/imdb-wiki-dir/data
 #CUDA_VISIBLE_DEVICES=0 python train.py --lr 0.0001 --groups 20 --epoch 200 --aug --soft_label --output_file aug_ --data_dir /home/rpu2/scratch/data/imbalanced-regression/imdb-wiki-dir/data
-CUDA_VISIBLE_DEVICES=1 python train.py --lr 0.0001 --groups 5 --epoch 150  -temp 0.07 --batch_size 256 --diversity 2 --sigma 0.1 --ranked_contra --soft_label --output_file softlabel_g5_0113 --data_dir /home/rpu2/scratch/data/imbalanced-regression/imdb-wiki-dir/data
+CUDA_VISIBLE_DEVICES=1 python train.py --lr 0.0001 --groups 5 --epoch 150 --ranked_contra --soft_label --output_file softlabel_g5_0113 --data_dir /home/rpu2/scratch/data/imbalanced-regression/imdb-wiki-dir/data
