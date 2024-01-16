fce0b75bb22a86e70941ca268b1b1d50eb25ab9f
ruizhipu <18651885620@163.com>
2024-01-15 20:59:08 -0500

    icml

--- agedb/train.py
+++ agedb/train.py
@@ -85,7 +85,7 @@ parser.add_argument('--temp', type=float, help='temperature for contrastive loss
 parser.add_argument('--contra_ratio', type=float, help='ratio fo contrastive loss', default=1)
 #
 parser.add_argument('--soft_label', action='store_true')
-parser.add_argument('--ce', action='store_false',  help='if use the cross_entropy /la or not')
+parser.add_argument('--ce', action='store_true',  help='if use the cross_entropy /la or not')
 parser.add_argument('--output_file', type=str, default='result_')
 parser.add_argument('--scale', type=float, default=1, help='scale of the sharpness in soft label')
 #parser.add_argument('--diversity', type=float, default=0, help='scale of the diversity loss in regressor output')
