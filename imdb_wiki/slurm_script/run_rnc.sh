ab3fe52dca3d0f923598c2040f790b45c6f1e271
ruizhipu <18651885620@163.com>
2024-01-18 21:51:45 -0500

    icml

--- imdb_wiki/slurm_script/run_rnc.sh
+++ imdb_wiki/slurm_script/run_rnc.sh
@@ -1,2 +1,24 @@
-CUDA_VISIBLE_DEVICES=2 python train_rnc.py --aug --epoch 400
-#CUDA_VISIBLE_DEVICES=2 python train_rnc.py --epoch 400
+#!/bin/bash
+#SBATCH --job-name=baselines
+#SBATCH --account=def-boyuwang
+#SBATCH --time=02-22:00
+#SBATCH --mem=32G
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=6
+#SBATCH --gres=gpu:1
+#SBATCH --mail-user=18651885620@163.com
+#SBATCH --mail-type=FAIL
+#SBATCH --output=slurm_output/slurm-%j-%x.out
+module load StdEnv/2020 cuda scipy-stack python/3.8
+#
+source /home/ruizhipu/envs/py38/bin/activate
+
+echo "SLURM_JOBID: " $SLURM_JOBID
+echo "SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
+echo "SLURM_ARRAY_JOB_ID: " $SLURM_ARRAY_JOB_ID
+
+
+#CUDA_VISIBLE_DEVICES=2 python train_rnc.py --aug --epoch 400
+python train_rnc.py --epoch 400
+
+
