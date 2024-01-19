6e63891f15ce5d74079cc5f8f9b1d494ac618246
ruizhipu <18651885620@163.com>
2024-01-18 22:27:47 -0500

    icml

--- imdb_wiki/test_dataset_vadility.py
+++ imdb_wiki/test_dataset_vadility.py
@@ -18,18 +18,23 @@ val_dataset = IMDBWIKI(data_dir=data_dirs, df=df_val,
                        img_size=224, split='val', group_num=10)
 test_dataset = IMDBWIKI(data_dir=data_dirs, df=df_test,
                         img_size=224, split='test', group_num=10)
+print(f'done dataset')
 train_loader = DataLoader(train_dataset,    batch_size=256,
                           shuffle=True, num_workers=8, pin_memory=True, drop_last=False)
 val_loader = DataLoader(val_dataset, batch_size=256, shuffle=False,
                         num_workers=8, pin_memory=True, drop_last=False)
 test_loader = DataLoader(test_dataset, batch_size=256, shuffle=False,
                          num_workers=8, pin_memory=True, drop_last=False)
-
+print(f'done dataloader')
 
 for idx, (inputs, targets, group) in enumerate(val_loader):
     inputs, targets, group = inputs.to(
         device), targets.to(device), group.to(device)
+    if idx % 100 == 0:
+        print(f' {idx} done in val')
 
 
 for idx, (x, y, g, w) in enumerate(train_loader):
     x, y, g = x.to(device), y.to(device), g.to(device)
+    if idx % 100 == 0:
+        print(f' {idx} done in val')
