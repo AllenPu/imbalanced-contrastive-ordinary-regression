20cdab46ef2b84b7efa81d4895478b631a933d25
ruizhipu <18651885620@163.com>
2024-01-23 00:30:19 -0500

    icml: train encoder

--- imdb_wiki/slurm_script/run_rnc.sh
+++ imdb_wiki/slurm_script/run_rnc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=baselines
 #SBATCH --account=def-boyuwang
-#SBATCH --time=03-00:00
+#SBATCH --time=05-00:00
 #SBATCH --mem=32G
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=6
