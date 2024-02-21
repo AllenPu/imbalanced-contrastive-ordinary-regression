359c91c9b64a752deea6c76a721a3408b4034fcf
AllenPu <18651885620@163.com>
2024-02-20 20:50:42 -0500

    debug

--- shanghai/image.py
+++ shanghai/image.py
@@ -29,7 +29,7 @@ def load_data(img_path,train = True):
             target = np.fliplr(target)
             img = img.transpose(Image.FLIP_LEFT_RIGHT)
     
-    print(f' target is {target.shape} img is {img.shape}')
+    print(f' target is {target.shape} target is {target}')
     print(f' shape 1 {target.shape[1]/8}, shape 0 is {target.shape[0]/8}')
  
     target = cv2.resize(target,(target.shape[1]/8,target.shape[0]/8),interpolation = cv2.INTER_CUBIC)*64
