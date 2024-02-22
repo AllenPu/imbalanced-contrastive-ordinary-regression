a491e89808ab4b257da23d652d6454784bf1d49d
ruizhipu <18651885620@163.com>
2024-02-22 04:09:28 -0500

    resume train rnc

--- agedb/train_rnc.py
+++ agedb/train_rnc.py
@@ -112,8 +112,10 @@ def train_epoch(model, train_loader, opt, args):
             y_output,  z = model(x)
             #
             y_ =  torch.chunk(y_output,2,dim=-1)
+            print(f' y shape {y_}')
             g_hat, y_hat = y_[0], y_[1]
             y_pred = torch.gather(y_hat, dim=1, index=g.to(torch.int64)) 
+            
             #
             if args.soft_label:
                 g_soft_label = soft_labeling(g, args).to(device)
