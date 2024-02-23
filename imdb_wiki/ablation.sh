dbdfbd6b47963c9e12a589e083164fe4f0bb48aa
ruizhipu <18651885620@163.com>
2024-02-23 02:53:31 -0500

    imdb:add ablation

---
+++ imdb_wiki/ablation.sh
@@ -0,0 +1,10 @@
+python train_rnc.py --epoch 30 --soft_label
+python train_rnc.py --epoch 40 --soft_label
+python train_rnc.py --epoch 50 --soft_label
+python train_rnc.py --epoch 60 --soft_label
+python train_rnc.py --epoch 40 --soft_label --lr 0.001
+python train_rnc.py --epoch 40 --soft_label --lr 0.005
+python train_rnc.py --epoch 40 --soft_label --lr 0.001 --groups 20
+python train_rnc.py --epoch 40 --soft_label --lr 0.001 --groups 25
+python train_rnc.py --epoch 40 --soft_label --lr 0.005 --groups 20
+python train_rnc.py --epoch 40 --soft_label --lr 0.005 --groups 25
