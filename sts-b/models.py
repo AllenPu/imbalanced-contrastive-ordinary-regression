66816ddd2c6fd78ff5f3debbf7fd696cfcd37777
ruizhipu <18651885620@163.com>
2024-02-25 02:23:44 -0500

    debug to sts

--- sts-b/models.py
+++ sts-b/models.py
@@ -194,7 +194,7 @@ class MultiTaskModel(nn.Module):
             #logits = torch.cat(pred_list) 
             #
             logits = logits.unsqueeze(-1)
-            #print(" logits shape ", logits.shape, " label shape ", label.shape )
+            print(" logits shape ", logits.shape, " label shape ", label.shape )
             assert logits.shape == label.shape
         else:
             if self.training and self.FDS is not None:
