22e1c6cee175d3d4cfa29e59b28cebd52edebc61
ruizhipu <18651885620@163.com>
2024-02-25 22:04:08 -0500

    store three criterion cls in agedb

--- agedb/ablation.sh
+++ agedb/ablation.sh
@@ -1,3 +1,3 @@
-python train_rnc.py --epoch 50 --ce --groups 20 --epuc 50
+python train_rnc.py --epoch 50 --ce --groups 20 --epoch 50
 python train_rnc.py --epoch 50 --soft_label --groups 20 --epoch 50
 python train_rnc.py --epoch 50 --la --groups 20 --epoch 50
