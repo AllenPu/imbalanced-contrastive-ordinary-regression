57b83a30d4e5a66c7451cb961c5e78e03191b326
AllenPu <18651885620@163.com>
2024-03-01 21:44:25 -0500

    imdb : count

--- imdb_wiki/utils.py
+++ imdb_wiki/utils.py
@@ -75,6 +75,8 @@ def shot_metric(pred, labels, train_labels, many_shot_thr=100, low_shot_thr=20):
     preds = np.hstack(pred)
     labels = np.hstack(labels)
     #
+    print(f' total length is {len(labels)}')
+    #
     train_labels = np.array(train_labels).astype(int)
     #
     train_class_count, test_class_count = [], []
@@ -113,8 +115,8 @@ def shot_metric(pred, labels, train_labels, many_shot_thr=100, low_shot_thr=20):
             median_shot_cnt.append(test_class_count[i])
 
     print(f'many_shot_l1 count  {np.sum(many_shot_cnt)}')
-    print(f'low_shot_l1 count  {np.sum(median_shot_cnt)}')
-    print(f'median_shot_l1 count {np.sum(low_shot_cnt)}')
+    print(f'median_shot_l1 count  {np.sum(median_shot_cnt)}')
+    print(f'low_shot_l1 count {np.sum(low_shot_cnt)}')
     #
     shot_dict = defaultdict(dict)
     shot_dict['many']['l1'] = np.sum(many_shot_l1) / np.sum(many_shot_cnt)
