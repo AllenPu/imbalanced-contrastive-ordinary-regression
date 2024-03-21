ac52b1600451916d072640ff6a51ca5e45c89e9c
AllenPu <18651885620@163.com>
2024-03-20 22:20:12 -0400

    synth

--- synthetic/utils.py
+++ synthetic/utils.py
@@ -93,8 +93,7 @@ def visualize(model_dict, train_loader, test_loader, Y_LB, Y_UB, K, B, store_nam
         df_ = make_dataframe(x_test, y, model_name)
         model_df.append(df_)
         print('here is ', df_['y'])
-        print(f' the {model_name} has the variance of y is {np.var(df_['y'])}')
-        assert 1==2
+        print(f' the {model_name} has the variance of y as {np.var(df_['y'])}')
         models = {}
         for trial in range(10):
             model.eval()
