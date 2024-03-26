05b1b5e27255b2592dbc09e4a946a6dc3c862f5a
ruizhipu <18651885620@163.com>
2024-03-25 23:56:32 -0400

    revise tsne

--- agedb/draw_tsne.py
+++ agedb/draw_tsne.py
@@ -2,8 +2,12 @@ from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 import argparse
 import torch
-from network import ResNet_regression
 from datasets.agedb import AgeDB
+from model import *
+import pandas as pd
+from torch.utils.data import DataLoader
+from collections import OrderedDict
+import os
 #from train import get_dataset
 
 
@@ -39,26 +43,73 @@ def draw_tsne(tsne_z_pred, tsne_g_pred, tsne_g_gt, args):
     plt.scatter(X_tsne_pred[:, 0], X_tsne_pred[:, 1],
                     c=tsne_g_gt, label="t-SNE true label")
     plt.legend()
-    plt.savefig('./images/tsne_x_pred_{}_sigma_{}_group_{}_model_{}_true_label.png'.format(
-        args.lr, args.sigma, args.groups, args.model_depth), dpi=120)
+    plt.savefig(f'./images/tsne_x_gt_group_{args.groups}_true_label.png', dpi=120)
     plt.figure(figsize=(10, 5))
     plt.scatter(X_tsne_pred[:, 0], X_tsne_pred[:, 1],
                     c=tsne_g_pred, label="t-SNE pred label")
     plt.legend()
-    plt.savefig('./images/tsne_x_pred_{}_sigma_{}_group_{}_model_{}_pred_lael.png'.format(
-        args.lr, args.sigma, args.groups, args.model_depth), dpi=120)
+    plt.savefig(f'./images/tsne_x_pred_group_{args.groups}_pred_lael.png', dpi=120)
     
+
+def get_data_loader(args):
+    print('=====> Preparing data...')
+    df = pd.read_csv(os.path.join(args.data_dir, "agedb.csv"))
+    df_train, df_val, df_test = df[df['split'] ==
+                                   'train'], df[df['split'] == 'val'], df[df['split'] == 'test']
+    train_labels = df_train['age']
+    #
+    train_dataset = AgeDB(data_dir=args.data_dir, df=df_train, img_size=args.img_size,
+                          split='train', reweight=args.reweight, group_num=args.groups)
+    #
+    group_list = train_dataset.get_group_list()
+    #
+    val_dataset = AgeDB(data_dir=args.data_dir, df=df_val,
+                        img_size=args.img_size, split='val', group_num=args.groups)
+    test_dataset = AgeDB(data_dir=args.data_dir, df=df_test,
+                         img_size=args.img_size, split='test', group_num=args.groups)
+    #
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True,
+                              num_workers=args.workers, pin_memory=True, drop_last=False)
+    val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False,
+                            num_workers=args.workers, pin_memory=True, drop_last=False)
+    test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False,
+                             num_workers=args.workers, pin_memory=True, drop_last=False)
+    print(f"Training data size: {len(train_dataset)}")
+    print(f"Validation data size: {len(val_dataset)}")
+    print(f"Test data size: {len(test_dataset)}")
+    return train_loader, val_loader, test_loader, group_list, train_labels
+
+
+def get_model(args):
+    model = Encoder_regression(groups=args.groups, name='resnet18')
+    # load pretrained
+    ckpt = torch.load('last.pth')
+    new_state_dict = OrderedDict()
+    for k,v in ckpt['model'].items():
+        key = k.replace('module.','')
+        keys = key.replace('encoder.','')
+        new_state_dict[keys]=v
+    model.encoder.load_state_dict(new_state_dict)
+    # freeze the pretrained part
+    #for (name, param) in model.encoder.named_parameters():
+    #    param.requires_grad = False
+    #
+    #optimizer = torch.optim.SGD(model.parameters(), lr=args.lr,
+    #                            momentum=args.momentum, weight_decay=args.weight_decay)
+    return model
+
+
+
 
 
 if __name__ == '__main__':
     exec('from train import get_dataset')
     args = parser.parse_args()
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    train_loader, test_loader, val_loader,  cls_num_list, train_labels = get_dataset(
-        args)
-    model = ResNet_regression(args).to(device)
-    model.load_state_dict(torch.load(
-        './models/model_{}.pth'.format(args.names)))
+    train_loader, test_loader, val_loader,  cls_num_list, train_labels = get_data_loader(args)
+    #
+    model = get_model(args)
+    model = model.to(device)
     tsne_z_pred = torch.Tensor(0)
     tsne_g_pred = torch.Tensor(0)
     tsne_g_gt = torch.Tensor(0)
