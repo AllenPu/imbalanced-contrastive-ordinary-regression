daabc58f6c71a9a3caae425ea7ba6390a56c59b5
ruizhipu <18651885620@163.com>
2024-03-30 01:58:32 -0400

    run all aug

--- agedb/batch.sh
+++ agedb/batch.sh
@@ -1,21 +1,3 @@
-for i in 0.00025 0.00035; do
-    for g in 10; do
-        for e in 450 500; do
-            for sigma in 1.5 1.8 2.1 2.2;do
-                for contra in True; do
-                    if [ $contra ];then
-                        for temp in 0.02 0.03; do
-                            jobs='contra_'+'lr'_${i}_'tau_0.5'_'group'_${g}_'epoch'_${e}_'temp'_${temp}
-                            echo ${jobs}
-                            sbatch --job-name=${jobs} ./slurm_jobs/train_contra.sh ${i} ${g} ${e} ${temp} ${sigma}
-                        done
-                    else
-                        jobs='lr'_${i}_'tau_0.5'_'group'_${g}_'epoch'_${e}
-                        echo ${jobs}
-                        sbatch --job-name=${jobs} ./slurm_jobs/train.sh ${i} ${g} ${e} ${sigma}
-                    fi 
-                done
-            done
-        done
-    done
+for i in 2 5 10 15 20 25 40 50; do
+    python train_rnc.py --groups $i --epoch 20 --soft_label
 done
