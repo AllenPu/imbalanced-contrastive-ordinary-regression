e51a1a0e80e5f88b94d1b854311df9220dda1e76
AllenPu <18651885620@163.com>
2024-04-02 21:38:57 -0400

    batch run

--- agedb/batch.sh
+++ agedb/batch.sh
@@ -1,5 +1,5 @@
 for i in 2 5 10 15 20 25 40 50; do
-    for j in 40, 50, 70; do
+    for j in 40 50 70; do
         echo 'the group is ' $i 'the rpoch is ' $j
         echo '----------------------'
         CUDA_VISIBLE_DEVICES=0 python train_rnc.py --groups $i --epoch $j --soft_label
