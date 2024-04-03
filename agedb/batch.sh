6194a4c0a874e5317e0524bceceba546d997a9fc
AllenPu <18651885620@163.com>
2024-04-02 21:16:21 -0400

    run all models

--- agedb/batch.sh
+++ agedb/batch.sh
@@ -3,3 +3,13 @@ for i in 2 5 10 15 20 25 40 50; do
     echo '----------------------'
     python train_rnc.py --groups $i --epoch 20 --soft_label
 done
+for i in 2 5 10 15 20 25 40 50; do
+    echo 'the group is ' $i
+    echo '----------------------'
+    python train_rnc.py --groups $i --epoch 20 --ce
+done
+for i in 2 5 10 15 20 25 40 50; do
+    echo 'the group is ' $i
+    echo '----------------------'
+    python train_rnc.py --groups $i --epoch 20 --la
+done
