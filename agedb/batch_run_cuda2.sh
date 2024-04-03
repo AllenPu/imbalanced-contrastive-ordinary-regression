8b223e534685296989c7e6115570dc6c349b59dc
AllenPu <18651885620@163.com>
2024-04-02 21:34:55 -0400

    batch run

--- agedb/batch_run_cuda2.sh
+++ agedb/batch_run_cuda2.sh
@@ -3,6 +3,6 @@ for i in 2 5 10 15 20 25 40 50; do
         echo 'the group is ' $i 'the rpoch is ' $j
         echo '----------------------'
         CUDA_VISIBLE_DEVICES=1 python train_rnc.py --groups $i --epoch $j --ce
-        ptrhon draw_tsne.py --groups $i --epoch $j --ce
+        python draw_tsne.py --groups $i --epoch $j --ce
     done
 done
--- agedb/batch_run_cuda3.sh
+++ agedb/batch_run_cuda3.sh
@@ -3,6 +3,6 @@ for i in 2 5 10 15 20 25 40 50; do
         echo 'the group is ' $i 'the rpoch is ' $j
         echo '----------------------'
         CUDA_VISIBLE_DEVICES=2 python train_rnc.py --groups $i --epoch $j --la
-        ptrhon draw_tsne.py --groups $i --epoch $j --la
+        python draw_tsne.py --groups $i --epoch $j --la
     done
 done
