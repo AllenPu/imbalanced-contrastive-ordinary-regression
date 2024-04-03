53fd98cc894ee6e238f3e277301bc4c27bd18964
AllenPu <18651885620@163.com>
2024-04-02 21:25:25 -0400

    batch run

--- agedb/batch.sh
+++ agedb/batch.sh
@@ -6,19 +6,4 @@ for i in 2 5 10 15 20 25 40 50; do
         ptrhon draw_tsne.py --groups $i --epoch $j
     done
 done
-for i in 2 5 10 15 20 25 40 50; do
-     for j in 40, 50, 70; do
-        echo 'the group is ' $i 'the rpoch is ' $j
-        echo '----------------------'
-        python train_rnc.py --groups $i --epoch $j --ce
-        ptrhon draw_tsne.py --groups $i --epoch $j --ce
-    done
-done
-for i in 2 5 10 15 20 25 40 50; do
-     for j in 40, 50, 70; do
-        echo 'the group is ' $i 'the rpoch is ' $j
-        echo '----------------------'
-        python train_rnc.py --groups $i --epoch $j --la
-        ptrhon draw_tsne.py --groups $i --epoch $j --la
-    done
-done
+
