c5affb193b6b60d612de7697a4a26aee7f873647
ruizhipu <18651885620@163.com>
2024-04-04 00:11:59 -0400

    add track on val

--- agedb/train_rnc_loss.py
+++ agedb/train_rnc_loss.py
@@ -172,12 +172,13 @@ def train_epoch(model, train_loader, val_loader, opt, args):
         print(f' At Epoch {e}, cls loss is {cls_loss.avg}, mse loss is {mse_loss.avg} val cls loss is {val_cls_loss.avg} val mse loss is {val_mse_loss.avg}')
     return model
 
-def train_epoch_single(model, train_loader, opt, args):
+def train_epoch_single(model, train_loader, val_loader, opt, args):
     model = model.to(device)
     model.train()
     mse = nn.MSELoss()   
     for e in tqdm(range(args.epoch)):
         mse_loss = AverageMeter()
+        val_mse_loss = AverageMeter()
         for idx, (x, y, g) in enumerate(train_loader):
             bsz = x.shape[0]
             x, y, g = x.to(device), y.to(device), g.to(device)
@@ -188,10 +189,17 @@ def train_epoch_single(model, train_loader, opt, args):
             mse_loss.update(loss_mse.item(), bsz)
             loss.backward()
             opt.step()
+        val_mse_loss = AverageMeter()
+        for idx, (x, y, g) in enumerate(val_loader):
+            x, y, g = x.to(device), y.to(device), g.to(device)
+            with torch.no_grad():
+                y_output,_ = model(x.to(torch.float32))
+                val_mse = F.mse_loss(y_output, y)
+                val_mse_loss.update(val_mse.item(), bsz)
         with open('./single_loss.csv', 'a', newline='') as f:
             writer = csv.writer(f)
             writer.writerow([e,mse_loss.avg])
-        print(f' At Epoch {e} single mse loss is {mse_loss.avg}')
+        print(f' At Epoch {e} single mse loss is {mse_loss.avg} val loss is {val_mse_loss.avg}')
     return model
 
 
