017cad329b3b42e89f369efe975e10e41438bb09
AllenPu <18651885620@163.com>
2024-04-05 20:33:04 -0400

    add

--- agedb/batch_loss.sh
+++ agedb/batch_loss.sh
@@ -1,4 +1,4 @@
-CUDACUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 150
-#CUDACUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --ce --epoch 150
+#CUDACUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 150
+CUDACUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --ce --epoch 150
 #CUDACUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --la --epoch 150
 #CUDACUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --soft_label --epoch 150
