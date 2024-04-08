150563f5a26c4f172361e03c5056fed681e59292
ruizhipu <18651885620@163.com>
2024-04-08 05:13:16 -0400

    test feature norm

--- agedb/ablation.sh
+++ agedb/ablation.sh
@@ -2,3 +2,4 @@
 #python train_rnc.py --epoch 50 --soft_label --groups 20 --epoch 50
 #python train_rnc.py --epoch 50 --la --groups 20 --epoch 50
 CUDACUDA_VISIBLE_DEVICES=2 python train_rnc.py --soft_label --epoch 150 --norm
+CUDACUDA_VISIBLE_DEVICES=2 python train_rnc.py --soft_label --epoch 150
