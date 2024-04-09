2131327ba316f4daf352d84225b284c64de3fa9c
ruizhipu <18651885620@163.com>
2024-04-09 02:16:16 -0400

    test feature norm for imdb

--- imdb_wiki/batch_loss.sh
+++ imdb_wiki/batch_loss.sh
@@ -1,2 +1,2 @@
-CUDA_VISIBLE_DEVICES=1 python train_rnc_loss.py --single_output --epoch 100  --norm --epoch 0
+CUDA_VISIBLE_DEVICES=1 python train_rnc_loss.py --single_output --epoch 100  --norm
 CUDA_VISIBLE_DEVICES=1 python train_rnc_loss.py --single_output --epoch 100
