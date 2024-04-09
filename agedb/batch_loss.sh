fff72e5eb9e1dd529ee4296884930810488668f6
ruizhipu <18651885620@163.com>
2024-04-09 03:17:53 -0400

    test feature norm for imdb

--- agedb/batch_loss.sh
+++ agedb/batch_loss.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 100  --norm
+CUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 0  --norm
 CUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 100
 CUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 100  --norm  --scratch
 CUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 100  --scratch
