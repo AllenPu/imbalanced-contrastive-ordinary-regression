8f39bc906b1159bebd0b55452ffc456497bb3193
ruizhipu <18651885620@163.com>
2024-04-22 08:37:19 -0400

    agedb

--- agedb/batch_loss.sh
+++ agedb/batch_loss.sh
@@ -1,7 +1,8 @@
 CUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 100  --oe
-CUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 100  --oe --weight_norm
-CUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 100  --oe --norm
-CUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 100  --oe --norm  --weight_norm
+CUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 100  --oe --pretrained
+CUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 100  --oe --weight_norm --pretrained
+CUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 100  --oe --norm --pretrained
+CUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 100  --oe --norm  --weight_norm --pretrained
 #CUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 100  --weight_norm
 #CUDACUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --ce --epoch 150 --lr 5e-4
 #CUDACUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --la --epoch 150
