697f70b0639823bf00262f4d34402fa141b930cf
ruizhipu <18651885620@163.com>
2024-04-22 22:54:59 -0400

    agedb

--- agedb/batch_loss.sh
+++ agedb/batch_loss.sh
@@ -9,11 +9,11 @@
 #CUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 100 --norm  --weight_norm --pretrained
 #CUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 100  --weight_norm
 #CUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 100  --oe 
-CUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 100  --oe --weight_norm 
-CUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 100  --oe --norm 
-CUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 100  --oe --norm  --weight_norm 
-echo " train on the pure OE+MSE loss"
-#CUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 100 --weight_norm 
-#CUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 100 --norm 
-#CUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 100 --norm  --weight_norm 
-#echo " train on the pure OE MSE"
+#CUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 100  --oe --weight_norm 
+#CUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 100  --oe --norm 
+#CUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 100  --oe --norm  --weight_norm 
+#echo " train on the pure OE+MSE loss"
+CUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 100 --weight_norm 
+CUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 100 --norm 
+CUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 100 --norm  --weight_norm 
+echo " train on the pure OE MSE"
