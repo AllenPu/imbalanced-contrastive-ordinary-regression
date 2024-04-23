4a1e0e488a682da7a8b29d343b99d72ddb2ca0c2
ruizhipu <18651885620@163.com>
2024-04-22 22:54:08 -0400

    agedb

--- agedb/batch_loss.sh
+++ agedb/batch_loss.sh
@@ -12,6 +12,8 @@
 CUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 100  --oe --weight_norm 
 CUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 100  --oe --norm 
 CUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 100  --oe --norm  --weight_norm 
+echo " train on the pure OE+MSE loss"
 #CUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 100 --weight_norm 
 #CUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 100 --norm 
 #CUDA_VISIBLE_DEVICES=2 python train_rnc_loss.py --single_output --epoch 100 --norm  --weight_norm 
+#echo " train on the pure OE MSE"
