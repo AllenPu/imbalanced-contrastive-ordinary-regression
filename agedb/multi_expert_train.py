bf82b4e618ce2175f9782bb749643e7b0b7a4888
AllenPu <18651885620@163.com>
2024-04-25 17:07:55 -0400

    agedb:debug

--- agedb/multi_expert_train.py
+++ agedb/multi_expert_train.py
@@ -128,6 +128,9 @@ def train_epoch(model, train_loader, val_loader, opt, args):
     mse = nn.MSELoss()
     model.train()
     maj_shot, med_shot, min_shot = shot_count(train_labels)
+    print(f' maj class {maj_shot}')
+    print(f' med class { med_shot}')
+    print(f' min class { min_shot}')
     for e in tqdm(range(args.epoch)):
         for idx, (x, y, group) in enumerate(train_loader):
             bsz = x.shape[0]
