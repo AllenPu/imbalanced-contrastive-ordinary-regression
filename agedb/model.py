2c177870063807e26e976e3ed60f87b3dc38e604
AllenPu <18651885620@163.com>
2024-04-25 19:03:55 -0400

    agedb:debug

--- agedb/model.py
+++ agedb/model.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import math
 import torch.nn.functional as F
-from datasets.agedb import shot_count
+
 
 def conv3x3(in_planes, out_planes, stride=1):
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
