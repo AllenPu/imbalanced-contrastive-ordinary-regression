345840012e5efdcd92666e4b5bebef7fcf6de587
ruizhipu <18651885620@163.com>
2024-07-14 19:55:51 +0800

    debug

--- agedb/uncertain_regression.py
+++ agedb/uncertain_regression.py
@@ -185,7 +185,7 @@ def train_epoch_uncertain(model, train_loader, val_loader, train_labels, opt, ar
             opt.zero_grad()
             pred, uncertain = model(x)
             #
-            loss = torch.mean( 0.5* torch.exp(-uncertain)*(y_pred - y)^2 + 0.5*torch.abs(uncertain-varianc))
+            loss = torch.mean( 0.5* torch.exp(-uncertain)*(pred - y)^2 + 0.5*torch.abs(uncertain-varianc))
             #
             #loss_mse = torch.pow(pred-y,2)
             #loss = torch.mean(loss_mse)
