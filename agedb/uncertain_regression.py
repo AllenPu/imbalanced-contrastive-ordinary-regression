cc974610d9acf743bb25650d14c66a5d485b6916
ruizhipu <18651885620@163.com>
2024-07-14 19:51:08 +0800

    debug

--- agedb/uncertain_regression.py
+++ agedb/uncertain_regression.py
@@ -175,8 +175,8 @@ def train_epoch_uncertain(model, train_loader, val_loader, train_labels, opt, ar
             bsz = x.shape[0]
             #
             #varianc_index = torch.LongTensor(y.squeeze(-1))
-            print(y.dtype)
-            varianc = var_tensor.index_select(0, index=y.squeeze(-1))
+            #print(y.dtype)
+            varianc = var_tensor.index_select(0, index=y.squeeze(-1).to(torch.int32))
             varianc = varianc.unsqueeze(-1)
             #
             x, y, g = x.cuda(non_blocking=True), y.cuda(non_blocking=True), g.cuda(non_blocking=True)
