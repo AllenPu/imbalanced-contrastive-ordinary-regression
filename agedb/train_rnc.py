c010857fbd8f03c000784b019f2c4df6a66354b4
ruizhipu <18651885620@163.com>
2024-08-23 22:59:34 +0800

    rescale soft label

--- agedb/train_rnc.py
+++ agedb/train_rnc.py
@@ -135,7 +135,7 @@ def train_epoch(model, train_loader, opt, args):
                 rescael_tensor = torch.Tensor(rescale_groups).repeat(bsz, 1).to(device)
                 # 
                 mask_1 = (g_soft_label == g_soft_label.max(dim=1, keepdim=True)[0])
-                # remove all current group index by multiple 0
+                # remove all current group index by multiple 0 (leave the max soft label then process others)
                 remove_1 = torch.where(mask_1, 1.0, 0.0)
                 remove_group_soft = g_soft_label * remove_1 * rescael_tensor
                 # remove all non current group index by multiple 0, reverse from above
