91b701c15a006fa206b71021ce889017c848b29a
ruizhipu <18651885620@163.com>
2024-09-21 12:37:06 +0800

    test the margin of the groups

--- agedb/batch.sh
+++ agedb/batch.sh
@@ -1,6 +1,7 @@
 #for i in 2 5 10 15 20 25 40 50; do
 #    python train_rnc.py --soft_label --asymm --groups $i
 #done
+
 for i in 0.5 0.7 0.9 1 1.25 1.5 2 4; do
     python train_rnc.py --soft_label --symm --step $i
 done
