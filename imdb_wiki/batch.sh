e89a8009d7628b71a116fb85d41296b965705dc2
ruizhipu <18651885620@163.com>
2024-09-23 01:39:40 +0800

    run imdb wiki

--- imdb_wiki/batch.sh
+++ imdb_wiki/batch.sh
@@ -1,5 +1,6 @@
-for i in 0.0001 0.0005 0.00001; do
+for e in 50 70 90; do
     #jobs='lr'_${i}_'tau_0.5'_'group'_${g}_'epoch'_${e}_'temp'_${temp}
     #echo ${jobs}
-    sbatch --job-name=${jobs} ./slurm_jobs/train.sh ${i} 
+    python train_rnc.py --model_name ./checkpoint/Supervised_group_lr_0.001_epoch_300_bs_128_groups_10.pth --epoch $e
+    #sbatch --job-name=${jobs} ./slurm_jobs/train.sh ${i} 
 done
