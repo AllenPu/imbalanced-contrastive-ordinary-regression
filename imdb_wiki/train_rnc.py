268dd0fa0cc49e93360a7dd1612cbc23dc0f9957
ruizhipu <18651885620@163.com>
2024-09-25 01:24:39 +0900

    run imdb wiki

--- imdb_wiki/train_rnc.py
+++ imdb_wiki/train_rnc.py
@@ -173,7 +173,7 @@ def train_epoch(model, train_loader, opt, args):
         if args.soft_label:
             g_soft_label = soft_labeling(g, args).to(device)
             if args.asymm:
-                    g_soft_label = asymmetric_soft_labeling(group_list, g_soft_label)
+                    g_soft_label = asymmetric_soft_labeling(train_group_cls_num, g_soft_label)
                 #print(f' g hat is {g_hat[:8]} soft label {g_soft_label[:8]}')
                 #assert 1== 2
             loss_ce = SoftCrossEntropy(g_hat, g_soft_label)
