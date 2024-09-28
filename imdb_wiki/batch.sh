c0fbc027c215b863b78ae4714de533d5e40fbba6
ruizhipu <18651885620@163.com>
2024-09-28 05:28:13 -0400

    reduce the lr in imdb

--- imdb_wiki/batch.sh
+++ imdb_wiki/batch.sh
@@ -5,5 +5,5 @@
     #sbatch --job-name=${jobs} ./slurm_jobs/train.sh ${i} 
 #done
 #python train_rnc.py --model_name ./checkpoint/Supervised_group_lr_0.001_epoch_300_bs_128_groups_10.pth --lr 5e-8 --encoder_lr 1e-10 --soft_label --asymm
-python train_rnc.py --model_name ./checkpoint/Supervised_group_lr_0.001_epoch_300_bs_128_groups_10.pth --lr 5e-9 --encoder_lr 1e-10 --soft_label --asymm
+CUDA_VISIBLE_DEVICES=1 python train_rnc.py --model_name ./checkpoint/Supervised_group_lr_0.001_epoch_300_bs_128_groups_10.pth --lr 5e-9 --encoder_lr 1e-10 --soft_label --asymm
 echo ' linear lr 5e-9 encoder 1e-10'
