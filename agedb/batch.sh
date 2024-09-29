050d4c30d56caa5f5bd3adeb770e4689542eb07d
ruizhipu <18651885620@163.com>
2024-09-29 16:02:07 -0400

    polish the tsne

--- agedb/batch.sh
+++ agedb/batch.sh
@@ -5,6 +5,7 @@
 #for i in 0.5 0.7 0.9 1 1.25 1.5 2 4; do
 #    python train_rnc.py --soft_label --step $i
 #done
-python draw_tsne.py --model_name ./checkpoint/groups_10_lr_0.001_epoch_100_soft_label_asymm.pth
-python draw_tsne.py --model_name ./checkpoint/groups_10_lr_0.001_epoch_100_soft_label_symm.pth
-python draw_tsne.py --model_name ./checkpoint/groups_10_lr_0.001_epoch_100_ce.pth
+
+python draw_tsne.py --model_name checkpoint/groups_10_lr_0.001_epoch_100_soft_label_asymm.pth
+python draw_tsne.py --model_name checkpoint/groups_10_lr_0.001_epoch_100_soft_label_symm.pth
+python draw_tsne.py --model_name checkpoint/groups_10_lr_0.001_epoch_100_ce.pth
