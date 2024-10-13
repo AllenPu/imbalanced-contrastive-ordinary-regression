410b2a18aaa3262448c9903466261aa9763d9088
ruizhipu <18651885620@163.com>
2024-10-12 22:01:50 -0400

    check difference in fine tune and linear prob

--- agedb/train_rnc.py
+++ agedb/train_rnc.py
@@ -291,6 +291,10 @@ def asymmetric_soft_labeling(group_list, g_soft_label):
 if __name__ == '__main__':
     args = parser.parse_args()
     setup_seed(args.seed)
+
+    #
+    print(f" =========the arg is {args.fine_tune}")
+    #
     train_loader, val_loader, test_loader, group_list, train_labels = get_data_loader(args)
     model, optimizer = get_model(args)
     store_name = 'groups_' + str(args.groups) + '_lr_' + str(args.lr) + '_epoch_' + str(args.epoch)
